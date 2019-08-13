db3964a20f0071f2d6d550c5086b1c679ec51947
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-08-13 11:14:38 +0200

    added validation for Balance

--- EncapsulationBankAccount.Entities/Account.cs
+++ EncapsulationBankAccount.Entities/Account.cs
@@ -6,35 +6,97 @@ namespace EncapsulationBankAccount.Entities
 {
     public class Account
     {
+        #region fields
         private int id;
         private decimal balance;
         private DateTime created;
-
+        #endregion
+        #region properties
         public int Id { get => id; set => id = value; }
-        public decimal Balance { get => balance; set => balance = value; }
-        public DateTime Created { get => created; set => created = value; }
 
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+            set
+            {
+                var validateResult = ValidateBalance(value);
+                if (!validateResult.isValid)
+                {
+                    throw new ArgumentException(validateResult.errorMessage);
+                }
+                else
+                {
+                    balance = value;
+                }
+               
+            }
+        }
+        public DateTime Created { get => created; set => created = value; }
+        #endregion
+        #region constructor
         public Account(int id, decimal balance, DateTime created)
         {
-            Id = id;
-            Balance = balance;
-            Created = created;
+            if (id == 0)
+            {
+                throw new ArgumentException("Id is 0");
+            }
+            else
+            {
+                Id = id;
+                Balance = balance;
+                Created = created;
+            }
         }
 
-        public Account (decimal initalBalance)
+        public Account(decimal initalBalance)
         {
             Balance = initalBalance;
+            Created = DateTime.Today;
         }
+        #endregion
+
+        public static (bool isValid, string errorMessage) ValidateBalance(decimal balance)
+        {
+            if (balance < -999999999 || balance > 999999999)
+            {
+                return (false, "balance is above 999999999 or below -999999999");
+            }
+            else
+            {
+                return (true, "balance is valid");
+            }
+        }
+
+
 
         public void Withdraw(decimal amount)
         {
-            Balance -= amount;
+            if (amount < 0 || amount > 25000)
+            {
+                throw new ArgumentException("Amount that is getting withdraw is more that 25000 or less than 0");
+            }
+            else
+            {
+                Balance -= amount;
+            }
         }
 
         public void Deposit(decimal amount)
         {
-            Balance += amount;
+            if (amount < 0 || amount > 25000)
+            {
+                throw new ArgumentException("Amount that is getting deposited is more that 25000 or less than 0");
+            }
+            else
+            {
+                Balance += amount;
+            }
         }
+
 
         public int GetDaysSinceCreation()
         {
