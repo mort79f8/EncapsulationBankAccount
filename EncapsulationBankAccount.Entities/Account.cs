6b5537b5e48ff363117d880a28c9585ce95fa9a7
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-08-13 09:58:22 +0200

    basic setup of the solution and project

---
+++ EncapsulationBankAccount.Entities/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationBankAccount.Entities
+{
+    public class Account
+    {
+        private int id;
+        private decimal balance;
+        private DateTime created;
+
+        public int Id { get => id; set => id = value; }
+        public decimal Balance { get => balance; set => balance = value; }
+        public DateTime Created { get => created; set => created = value; }
+
+        public Account(int id, decimal balance, DateTime created)
+        {
+            Id = id;
+            Balance = balance;
+            Created = created;
+        }
+
+        public Account (decimal initalBalance)
+        {
+            Balance = initalBalance;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            throw NotImplementedException;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            throw NotImplementedException;
+        }
+
+        public int GetDaysSinceCreation()
+        {
+            throw NotImplementedException;
+        }
+    }
+}
