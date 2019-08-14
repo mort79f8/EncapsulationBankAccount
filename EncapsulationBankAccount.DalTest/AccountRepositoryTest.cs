c0b01a74f05f75b79ebe9720ce808ab20a5bfaa0
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-08-14 14:34:18 +0200

    Added Index page to show total balance of all accounts.

--- EncapsulationBankAccount.DalTest/AccountRepositoryTest.cs
+++ EncapsulationBankAccount.DalTest/AccountRepositoryTest.cs
@@ -2,6 +2,8 @@ using EncapsulationBankAccount.Dal;
 using EncapsulationBankAccount.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using Xunit;
 
 namespace EncapsulationBankAccount.DalTest
@@ -11,7 +13,54 @@ namespace EncapsulationBankAccount.DalTest
         [Fact]
         public void GetAll_IsTrueIfNumbersIsEqual()
         {
+            //Arrange
+            AccountRepository accountRepository = new AccountRepository();
+            List<Account> accounts = accountRepository.GetAll();
 
+            //Act
+            int actualAmountOfAccounts = NumberOfAccounts();
+
+            //Assert
+            Assert.Equal(accounts.Count, actualAmountOfAccounts);
         }
+
+        private int NumberOfAccounts()
+        {
+            int count = 0;
+            string sql = "SELECT COUNT(Id) FROM Accounts";
+            DataTable dataTable = ExecuteQuery(sql);
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                count = (int)row[0];
+            }
+
+            
+            return count;
+        }
+
+        #region Database Connection
+        private string connectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=EncapsulationDatabase;Integrated Security=true;";
+        protected DataTable ExecuteQuery(string sql)
+        {
+            DataTable dataTable = new DataTable(); //The dataset to be returned
+
+            //Uses the connection and command
+            //The using makes sure to close and dispose the objects in the end
+            using (var con = new SqlConnection(connectionString))
+            using (var command = new SqlCommand(sql, con))
+            {
+                //Open the connection
+                con.Open();
+                //Get the data from the database
+                SqlDataAdapter da = new SqlDataAdapter(command);
+
+                da.Fill(dataTable);
+            }
+            return dataTable;
+        }
+        #endregion
     }
+
+
 }
---
+++ EncapsulationBankAccount.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EncapsulationBankAccount.Dal;
+using EncapsulationBankAccount.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EncapsulationBankAccount.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public Account account { get; set; }
+        public List<Account> accounts { get; set; } = new List<Account>();
+
+        public IActionResult OnGet()
+        {
+            return InitializeData();
+        }
+
+        public IActionResult InitializeData()
+        {
+            AccountRepository accountRepository = new AccountRepository();
+            try
+            {
+                accounts = accountRepository.GetAll();
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public decimal CalcTotalBalance(List<Account> accounts)
+        {
+            decimal total = 0;
+            foreach (var account in accounts)
+            {
+                total += account.Balance;
+            }
+            return total;
+        }
+    }
+}
