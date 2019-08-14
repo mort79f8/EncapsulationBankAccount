4e362c4eac24c30f110cf94b9d2059d0c9e6a54b
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-08-14 13:14:39 +0200

    started on AccountRepository test is abit stuck.

---
+++ EncapsulationBankAccount.DalTest/AccountRepositoryTest.cs
@@ -0,0 +1,17 @@
+using EncapsulationBankAccount.Dal;
+using EncapsulationBankAccount.Entities;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace EncapsulationBankAccount.DalTest
+{
+    public class AccountRepositoryTest
+    {
+        [Fact]
+        public void GetAll_IsTrueIfNumbersIsEqual()
+        {
+
+        }
+    }
+}
