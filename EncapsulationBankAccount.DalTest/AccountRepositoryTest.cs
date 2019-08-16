2dc635c3ff4a281a0b6da876d83716ccf25827ed
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-08-16 09:48:27 +0200

    continue with testing Dal.

--- EncapsulationBankAccount.DalTest/AccountRepositoryTest.cs
+++ EncapsulationBankAccount.DalTest/AccountRepositoryTest.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using Xunit;
 
 namespace EncapsulationBankAccount.DalTest
@@ -24,22 +25,69 @@ namespace EncapsulationBankAccount.DalTest
             Assert.Equal(accounts.Count, actualAmountOfAccounts);
         }
 
-        private int NumberOfAccounts()
+        //public void GetAccount_IsEqualWhenBothAccountIdIsTheSame()
+        //{
+        //    //Arrange
+
+        //    //Act
+
+
+        //    //Assert
+        //}
+
+        [Fact]
+        public void Update_IsEqualIfAccountGetsUpdated()
         {
-            int count = 0;
-            string sql = "SELECT COUNT(Id) FROM Accounts";
-            DataTable dataTable = ExecuteQuery(sql);
+            //Arrange
+            Account newAccount = new Account(1500);
+            AccountRepository accountRepository = new AccountRepository();
+            accountRepository.Insert(newAccount);
+            Account account = accountRepository.GetAccount(SelectLatestAccount().Id);
 
-            foreach (DataRow row in dataTable.Rows)
-            {
-                count = (int)row[0];
-            }
-
+            //Act
+            account.Balance = 5000;
+            accountRepository.Update(account);
+            Account updatedAccount = accountRepository.GetAccount(account.Id);
             
-            return count;
+
+            //Assert
+            Assert.Equal(account, updatedAccount);
         }
 
-        #region Database Connection
+        [Fact]
+        public void Insert_IsEqualWhenAnAccountIsInserted()
+        {
+            //Arrange
+            Account account = new Account(30000);
+            AccountRepository accountRepository = new AccountRepository();
+            int currentNumberOfAccounts = NumberOfAccounts();
+
+            //Act
+            accountRepository.Insert(account);
+            int numberOfAccountsAfterInsert = NumberOfAccounts();
+
+            //Assert
+            Assert.Equal(currentNumberOfAccounts + 1, numberOfAccountsAfterInsert);
+        }
+
+        [Fact]
+        public void Delete_NumberOfAccountIsReduceByOne()
+        {
+            //Arrange
+            int currentNumberOfAccount = NumberOfAccounts();
+            Account account = SelectLatestAccount();
+            AccountRepository accountRepository = new AccountRepository();
+
+            //Act
+            accountRepository.Delete(account.Id);
+            int NumberOfAccountsAfterDelete = NumberOfAccounts();
+
+            //Assert
+            Assert.Equal(currentNumberOfAccount - 1, NumberOfAccountsAfterDelete);
+        }
+
+
+        #region Database Connection and Data Handling
         private string connectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=EncapsulationDatabase;Integrated Security=true;";
         protected DataTable ExecuteQuery(string sql)
         {
@@ -58,6 +106,50 @@ namespace EncapsulationBankAccount.DalTest
                 da.Fill(dataTable);
             }
             return dataTable;
+        }
+
+        private int NumberOfAccounts()
+        {
+            int count = 0;
+            string sql = "SELECT COUNT(Id) FROM Accounts";
+            DataTable dataTable = ExecuteQuery(sql);
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                count = (int)row[0];
+            }
+
+
+            return count;
+        }
+
+        private Account SelectLatestAccount()
+        {
+            string sql = "SELECT TOP 1 * FROM Accounts ORDER BY ID DESC";
+            DataTable dataTable = ExecuteQuery(sql);
+            if (dataTable == null)
+                throw new InvalidOperationException($"DataTable was null. SQL string is: {sql}");
+            return HandleData(dataTable).FirstOrDefault();
+
+        }
+
+        private List<Account> HandleData(DataTable dataTable)
+        {
+            if (dataTable == null)
+                return null;
+
+            List<Account> accounts = new List<Account>();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                accounts.Add(new Account()
+                {
+                    Id = (int)row["Id"],
+                    Balance = (decimal)row["Balance"],
+                    Created = (DateTime)row["Created"]
+                });
+            }
+            return accounts;
         }
         #endregion
     }
